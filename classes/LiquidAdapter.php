ad64593327a2727333e52c3ddd21d102a1462989
Alex Morega <alex@grep.ro>
2018-03-20 18:52:41 +0200

    oauth implementation for liquid

---
+++ classes/LiquidAdapter.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace OAuth\Plugin;
+
+use OAuth\OAuth2\Service\Liquid;
+
+class LiquidAdapter extends AbstractAdapter {
+
+    /**
+     * Retrieve the user's data
+     *
+     * The array needs to contain at least 'user', 'mail', 'name' and optional 'grps'
+     *
+     * @return array
+     */
+    public function getUser() {
+        $JSON = new \JSON(JSON_LOOSE_TYPE);
+        $data = array();
+
+        /** var OAuth\OAuth2\Service\Generic $this->oAuth */
+        $result = $JSON->decode($this->oAuth->request('http://liquid.example.org/accounts/profile'));
+
+        $data['user'] = $result['login'];
+        $data['mail'] = $result['login'] . '@localhost';
+
+        return $data;
+    }
+
+    /**
+     * We make use of the "Generic" oAuth 2 Service as defined in
+     * phpoauthlib/src/OAuth/OAuth2/Service/Generic.php
+     *
+     * @return string
+     */
+    public function getServiceName() {
+        return 'Liquid';
+    }
+
+    public function getScope() {
+        return array('read');
+    }
+
+}
---
+++ phpoauthlib/src/OAuth/OAuth2/Service/Liquid.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace OAuth\OAuth2\Service;
+
+use OAuth\OAuth2\Token\StdOAuth2Token;
+use OAuth\Common\Http\Exception\TokenResponseException;
+use OAuth\Common\Http\Uri\Uri;
+use OAuth\Common\Consumer\CredentialsInterface;
+use OAuth\Common\Http\Client\ClientInterface;
+use OAuth\Common\Storage\TokenStorageInterface;
+use OAuth\Common\Http\Uri\UriInterface;
+
+class Liquid extends AbstractService
+{
+    const SCOPE_READ = 'read';
+
+    public function getAuthorizationEndpoint()
+    {
+        return new Uri('http://liquid.example.org/o/authorize/');
+    }
+
+    public function getAccessTokenEndpoint()
+    {
+        return new Uri('http://liquid.example.org/o/token/');
+    }
+
+    protected function getAuthorizationMethod()
+    {
+        return static::AUTHORIZATION_METHOD_HEADER_BEARER;
+    }
+
+    protected function parseAccessTokenResponse($responseBody)
+    {
+        $data = json_decode($responseBody, true);
+
+        if (null === $data || !is_array($data)) {
+            throw new TokenResponseException('Unable to parse response.');
+        } elseif (isset($data['error'])) {
+            throw new TokenResponseException('Error in retrieving token: "' . $data['error'] . '"');
+        }
+
+        $token = new StdOAuth2Token();
+        $token->setAccessToken($data['access_token']);
+
+        if (isset($data['expires_in'])) {
+            $token->setLifeTime($data['expires_in']);
+        }
+
+        if (isset($data['refresh_token'])) {
+            $token->setRefreshToken($data['refresh_token']);
+            unset($data['refresh_token']);
+        }
+
+        unset($data['access_token']);
+        unset($data['expires_in']);
+
+        $token->setExtraParams($data);
+
+        return $token;
+    }
+}
